a8dcee24e3bece8d6f69b877f84fb69eb3c8339d
martin <martin.delatorre@monterrey.gob.mx>
2024-01-31 17:37:33 -0600

    errores en importaciones

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -41,5 +41,167 @@ import {StyleManager} from 'oltb/src/oltb/js/managers/StyleManager'
 import {LayerManager} from 'oltb/src/oltb/js/managers/LayerManager';
 import {StateManager} from 'oltb/src/oltb/js/managers/StateManager'
 import {TippyManager} from 'oltb/src/oltb/js/managers/TippyManager'
+import {ErrorManager} from 'oltb/src/oltb/js/managers/ErrorManager'
+import {ConfigManager} from 'oltb/src/oltb/js/managers/ConfigManager'
+import {ElementManager} from 'oltb/src/oltb/js/managers/ElementManager'
+import {TooltipManager} from 'oltb/src/oltb/js/managers/TooltipManager'
+import {FeatureManager} from 'oltb/src/oltb/js/managers/FeatureManager';
+import {SettingsManager} from 'oltb/src/oltb/js/managers/SettingsManager'
+import {BootstrapManager} from 'oltb/src/oltb/js/managers/BootstrapManager'
+import {InfoWindowManager} from 'oltb/src/oltb/js/managers/InfoWindowManager'
+import {ProjectionManager} from 'oltb/src/oltb/js/managers/ProjectionManager'
+import {TranslationManager} from 'oltb/src/oltb/js/managers/TranslationManager'
+import {ColorPickerManager} from 'oltb/src/oltb/js/managers/ColorPickerManager'
+import {AccessibilityManager} from 'oltb/src/oltb/js/managers/AccessibilityManager'
 
+// Toolbar Tools
+import {HomeTool} from 'oltb'
+import {HiddenMarkerTool} from 'oltb/src/oltb/js/tools/hidden-tools/HiddenMarkerTool'
+import {HiddenMapNavigationTool} from 'oltb/src/oltb/js/tools/hidden-tools/HiddenMapNavigationTool'
+import {HiddenAboutTool} from 'oltb/src/oltb/js/tools/hidden-tools/HiddenAboutTool'
+
+BootstrapManager.initAsync([
+  { manager: LogManager },
+    { manager: StyleManager },
+    { manager: ErrorManager },
+    { manager: FeatureManager },
+    { manager: StateManager, options: {
+        ignoredKeys: []
+    }},
+    { manager: ElementManager },
+    { manager: ConfigManager },
+    { manager: TranslationManager },
+    { manager: ProjectionManager },
+    { manager: LayerManager },
+    { manager: ColorPickerManager },
+    { manager: TippyManager },
+    { manager: TooltipManager },
+    { manager: UrlManager },
+    { manager: ToolManager },
+    { manager: SettingsManager },
+    { manager: SnapManager },
+    { manager: InfoWindowManager },
+    { manager: AccessibilityManager }
+]).then(() => {
+    initMapAndToolbar();
+});
+const initMapAndToolbar = () => {
+    const defaultLocation = ConfigManager.getConfig().location.default;
+    const localStorageNodeName = LocalStorageKeys.mapData;
+    const localStorageDefaults = Object.freeze({
+        lon: defaultLocation.lon,
+        lat: defaultLocation.lat,
+        zoom: defaultLocation.zoom,
+        rotation: defaultLocation.rotation,
+    });
+
+    const localStorage = StateManager.getAndMergeStateObject(
+        localStorageNodeName,
+        localStorageDefaults
+    );
+
+    const defaultProjection = ConfigManager.getConfig().projection.default;
+    const map = new Map({
+        interactions: defaultInterctions({
+            mouseWheelZoom: false,
+            altShiftDragRotate: false,
+            dragPan: false,
+            keyboard: false
+        }).extend([
+            new MouseWheelZoom({
+                condition: function(event) {
+                    return (
+                        platformModifierKeyOnly(event) ||
+                        SettingsManager.getSetting(Settings.mouseWheelZoom)
+                    );
+                }
+            }),
+            new DragRotate({
+                condition: function(event) {
+                    return (
+                        altShiftKeysOnly(event) &&
+                        SettingsManager.getSetting(Settings.altShiftDragRotate)
+                    );
+                }
+            }),
+            new DragPan({
+                condition: function(event) {
+                    return (
+                        (
+                            platformModifierKeyOnly(event) ||
+                            SettingsManager.getSetting(Settings.dragPan)
+                        ) && !altShiftKeysOnly(event) && !shiftKeyOnly(event)
+                    );
+                }
+            }),
+            new KeyboardZoom({
+                condition: function(event) {
+                    return (
+                        SettingsManager.getSetting(Settings.keyboardZoom) &&
+                        targetNotEditable(event)
+                    );
+                }
+            }),
+            new KeyboardPan({
+                condition: function(event) {
+                    return (
+                        SettingsManager.getSetting(Settings.keyboardPan) &&
+                        targetNotEditable(event)
+                    );
+                }
+            })
+        ]),
+        controls: defaultControls({
+            zoom: false,
+            rotate: false
+        }).extend([
+            new HiddenMarkerTool({
+                onAdded: function(marker: any) {
+                    console.log('HiddenMarkerTool: Marker added', marker);
+                },
+                onRemoved: function(marker: any) {
+                    console.log('HiddenMarkerTool: Marker removed', marker);
+                },
+                onEdited: function(before: any, after: any) {
+                    console.log('HiddenMarkerTool: Marker edited', before, after);
+                }
+            }),
+            new HiddenMapNavigationTool({
+                focusZoom: 10
+            }),
+            new HomeTool({
+                lon: 18.1201,
+                lat: 35.3518,
+                zoom: 3,
+                onInitiated: function() {
+                    console.log('HomeTool: Initiated');
+                },
+                onClicked: function() {
+                    console.log('HomeTool: Clicked');
+                },
+                onBrowserStateCleared: function() {
+                    console.log('HomeTool: State cleared');
+                },
+                onNavigatedHome: function(result: any) {
+                    console.log('HomeTool: Navigated home', result);
+                }
+            }),
+            new HiddenAboutTool(),
+            new ContextMenu()
+        ]),
+        target: ElementManager.getMapElement(),
+        view: new View({
+            projection: getProjection(defaultProjection),
+            center: fromLonLat([
+                Number(localStorage.lon),
+                Number(localStorage.lat)
+            ], defaultProjection),
+            zoom: localStorage.zoom,
+            rotation: localStorage.rotation
+        })
+    });
+
+    BootstrapManager.setMap(map);
+    BootstrapManager.ready();
+}
 </script>
