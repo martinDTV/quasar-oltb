23140e9cce59842464c96b6baf76eaf5116a27b0
Tatiana Garcia <108375769+DraCaligari@users.noreply.github.com>
2024-02-01 09:02:48 -0600

    declare module's

---
+++ src/declare_modules/oltb.d.ts
@@ -0,0 +1,34 @@
+// Core Toolbar
+declare module 'oltb/src/oltb/js/helpers/constants/Settings';
+declare module 'oltb/src/oltb/js/common/ContextMenu';
+declare module 'oltb/src/oltb/js/helpers/constants/LocalStorageKeys';
+
+// Core Managers
+declare module 'oltb/src/oltb/js/managers/LogManager';
+declare module 'oltb/src/oltb/js/managers/UrlManager';
+declare module 'oltb/src/oltb/js/managers/ToolManager';
+declare module 'oltb/src/oltb/js/managers/SnapManager';
+declare module 'oltb/src/oltb/js/managers/StyleManager';
+declare module 'oltb/src/oltb/js/managers/LayerManager';
+declare module 'oltb/src/oltb/js/managers/StateManager';
+declare module 'oltb/src/oltb/js/managers/TippyManager';
+declare module 'oltb/src/oltb/js/managers/ErrorManager';
+declare module 'oltb/src/oltb/js/managers/ConfigManager';
+declare module 'oltb/src/oltb/js/managers/ElementManager';
+declare module 'oltb/src/oltb/js/managers/TooltipManager';
+declare module 'oltb/src/oltb/js/managers/FeatureManager';
+declare module 'oltb/src/oltb/js/managers/SettingsManager';
+declare module 'oltb/src/oltb/js/managers/BootstrapManager';
+declare module 'oltb/src/oltb/js/managers/InfoWindowManager';
+declare module 'oltb/src/oltb/js/managers/ProjectionManager';
+declare module 'oltb/src/oltb/js/managers/TranslationManager';
+declare module 'oltb/src/oltb/js/managers/ColorPickerManager';
+declare module 'oltb/src/oltb/js/managers/AccessibilityManager';
+
+// Toolbar Tools
+declare module 'oltb/src/oltb/js/tools/HomeTool';
+declare module 'oltb/src/oltb/js/tools/hidden-tools/HiddenMarkerTool';
+declare module 'oltb/src/oltb/js/tools/hidden-tools/HiddenMapNavigationTool';
+declare module 'oltb/src/oltb/js/tools/hidden-tools/HiddenAboutTool';
+
+
