18832c7b63df1e8aee8413348107fc5b9b815622
martin <martin.delatorre@monterrey.gob.mx>
2024-02-01 16:38:37 -0600

    Se agrego el mapa al visualizar junto con 3 herramientas

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <q-footer class="footer bg-white">
+      <q-toolbar class="full-height">
+        <q-toolbar-title class="text-black toolbar-title" style="font-size: 13px; text-align: center;">
+          Secretaria de Innovación y Gobierno Abierto 2024
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.footer {
+  height: 25px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.toolbar-title {
+  text-align: center;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-header  class="bg-white header">
+      <q-toolbar>
+        <q-toolbar-title class="text-black" style="font-size: 1.8rem">
+          MIRA+
+        </q-toolbar-title>
+
+          <q-icon
+            class="icon-coustumer"
+            name="account_circle"
+            color="dark"
+            size="35px"
+          />
+      </q-toolbar>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.header{
+  height: 50px;
+}
+.icon-coustumer{
+  cursor: pointer;
+  margin-right: .2rem;
+}
+</style>
